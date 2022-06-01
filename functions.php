53d21e077f84f7f6f9345f7ed9f3fb91c5c82836
Scott Shealy <vsshealy@gmail.com>
2022-06-01 08:34:17 -0400

    Added minimum theme files

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+    /**
+     * functions.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0
+     * @copyright 2022
+    **/
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    /**
+     * index.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0
+     * @copyright 2022
+    **/
+?>
